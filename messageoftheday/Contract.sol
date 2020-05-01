b636610ae5141407accc7f56a79b1fc942daed83
vicsuka <horvath.mark@lightport.hu>
2020-05-01 18:08:18 +0200

    Added basic smart contract outline

--- messageoftheday/Contract.sol
+++ messageoftheday/Contract.sol
@@ -2,23 +2,44 @@ pragma solidity ^0.6.0;
 
 import "../Ownable.sol";
 
-contract MessageOfTheMoment is Ownable {
-    string public message;
-    uint public maxLength;
+contract TripWithEthereum is Ownable {
+    
+    struct Trip {
+      address  organizer;
+      uint  price;
+      uint  maxPeople;
+      address[]  participants;
+      string status;
+    }
+    
+    mapping(string => Trip) public trips;
 
+    
     constructor() public {
-        message = "Hello World";
-        maxLength = 280;
     }
 
-    function setMessage(string memory _message) public {
-        require(bytes(_message).length <= maxLength, "That message is too long.");
+    function createTrip(string memory uuid, address organizer, uint price, uint maxPeople) public {
+        Trip memory newTrip;
         
-        message = _message;
+        newTrip.organizer = organizer;
+        newTrip.price = price;
+        newTrip.maxPeople = maxPeople;
+        newTrip.status = "ORGANIZING";
+        
+        //= Trip(organizer,price,maxPeople,[],Status.ORGANIZING);
+        
+        trips[uuid] = newTrip;
     }
 
-    function setMaxLength(uint _maxLength) public onlyOwner {
-        maxLength = _maxLength;
+    
+    function getTrip(string calldata uuid) external view returns (address ,uint,uint, address[] memory,string memory) {
+        Trip memory toReturn = trips[uuid];
+        return (toReturn.organizer,
+                toReturn.price,
+                toReturn.maxPeople,
+                toReturn.participants,
+                toReturn.status
+        );
     }
 
 }
